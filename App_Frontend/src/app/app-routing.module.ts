f7a05f5f2274ef720b9d012351b152ef4fd3d2cb
sukanyaharidas <sukanyaharidas97@gmail.com>
2022-07-15 17:14:33 +0530

    template update

--- App_Frontend/src/app/app-routing.module.ts
+++ App_Frontend/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { Template1Component } from './template1/template1.component';
 
 const routes: Routes = [{path:"",component: HomeComponent},
                         {path:"\login",component: LoginComponent},
-                        {path:"\signup",component: SignupComponent}]
+                        {path:"\signup",component: SignupComponent},
+                        {path:"\_temp1",component: Template1Component}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- App_Frontend/src/app/template1/template1.component.ts
+++ App_Frontend/src/app/template1/template1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-template1',
   templateUrl: './template1.component.html',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Template1Component implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  ngOnInit() {
   }
+ 
 
 }
