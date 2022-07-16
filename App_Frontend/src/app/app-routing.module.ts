90f9b4aafdddd41c70cccca6895fc4b5d1715e89
sukanyaharidas <sukanyaharidas97@gmail.com>
2022-07-16 17:59:59 +0530

    Update app-routing.module.ts

--- App_Frontend/src/app/app-routing.module.ts
+++ App_Frontend/src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { Template1Component } from './template1/template1.component';
 import { Template2Component } from './template2/template2.component';
+import { Template3Component } from './template3/template3.component';
 
 const routes: Routes = [{path:"",component: HomeComponent},
                         {path:"\login",component: LoginComponent},
                         {path:"\signup",component: SignupComponent},
                         {path:"\_temp1",component: Template1Component},
                         {path:"\data",component: Form1Component},
-                        {path:"\_temp2",component:Template2Component}]
+                        {path:"\_temp2",component:Template2Component},
+                        {path:"\_temp3",component:Template3Component}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
