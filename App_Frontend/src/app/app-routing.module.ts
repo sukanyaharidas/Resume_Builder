3dbf865889535971b3c162b49e95cb2563ed9f74
binilpb <PB.BINIL@GMAIL.COM>
2022-07-22 03:44:30 +0530

    Update app-routing.module.ts

--- App_Frontend/src/app/app-routing.module.ts
+++ App_Frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChooseTemmplateComponent } from './choose-temmplate/choose-temmplate.component';
 import { Form1Component } from './form1/form1.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+// import { ResumePreviewComponent } from './resume-preview/resume-preview.component';
 import { SignupComponent } from './signup/signup.component';
 import { Template1Component } from './template1/template1.component';
 import { Template2Component } from './template2/template2.component';
@@ -14,7 +16,18 @@ const routes: Routes = [{path:"",component: HomeComponent},
                         {path:"\_temp1",component: Template1Component},
                         {path:"\data",component: Form1Component},
                         {path:"\_temp2",component:Template2Component},
-                        {path:"\_temp3",component:Template3Component}]
+                        {path:"\_temp3",component:Template3Component},
+                        {path:"\Choose_Template",component:ChooseTemmplateComponent},
+                        // {path:"\previewResume",
+                        //  component:ResumePreviewComponent,
+                        // children:[
+                        //   {path:"\_temp1",component: Template1Component},
+                        //   {path:"\data",component: Form1Component},
+                        //   {path:"\_temp2",component:Template2Component},
+                        //   {path:"\_temp3",component:Template3Component}
+                        //   ] 
+                        // }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
