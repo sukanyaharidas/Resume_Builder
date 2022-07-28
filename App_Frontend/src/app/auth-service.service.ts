235ff031e9725f749c0943ab7ef8011414995541
sukanyaharidas <sukanyaharidas97@gmail.com>
2022-07-28 12:49:03 +0530

    Update auth-service.service.ts

--- App_Frontend/src/app/auth-service.service.ts
+++ App_Frontend/src/app/auth-service.service.ts
@@ -5,24 +5,46 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class AuthServiceService {
-
+ Adminrole=false;
+ Userrole=false;
   constructor(public http:HttpClient) { }
 
   userSignup(users:any){
     console.log(users);
-    return this.http.post('http://localhost:3000/signup', {users})
-    .subscribe((data)=>console.log(data));
+    return this.http.post<any>('http://localhost:4000/signup', {users});
     }
 
 
   login(authData:any){
     console.log(authData);
-    return this.http.post('http://localhost:3000/login',{authData});
+    return this.http.post<any>('http://localhost:4000/login',{authData});
   
     }
 
-}
-function subscribe(arg0: (data: any) => void) {
-  throw new Error('Function not implemented.');
-}
+    adminLogin(data:any){
+      console.log(data);
+      return this.http.post<any>('http://localhost:4000/login_admin', {data});
+    }
 
+
+    loggedIn(){
+      return !!localStorage.getItem('token');
+    }
+
+    getToken(){
+      return  localStorage.getItem('token');
+    }
+
+
+    getAdminrole(){
+      this.Adminrole=true;
+      this.Userrole=false;
+    }
+    getUserrole(){
+      this.Userrole=true;
+      this.Adminrole=false;
+    }
+// function subscribe(arg0: (data: any) => void) {
+//   throw new Error('Function not implemented.');
+// }
+}
